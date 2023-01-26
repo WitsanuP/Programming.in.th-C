b0aa7f36162b7700a3ab4bfd3ccb90b13f8f67e2
WitsanuP <polpoillopi@gmail.com>
2023-01-26 21:36:12 +0700

    rename

---
+++ 0000_A+B_Problem.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",a+b);
+
+}
+
+
+/*
+Data type
+Primary
+-integer (int 2,147,483,674 %d)
+-charater
+-Floating Point
+-Double Floating Point
+-Void
+
+Derived
+-Function
+-Array
+-Pointer
+-Reference
+
+User Defined
+-Class
+-Structure
+-Union
+-Enum
+-Typedef
+
+*/
